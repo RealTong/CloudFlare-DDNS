001bc41bd3367fbada5bd370aa6f4e7de0e3d5b6
WULANREN <me@wushutong.cn>
2021-08-20 15:30:26 +0800

    add some content 🤞

--- CloudFlare-DDNS.sh
+++ CloudFlare-DDNS.sh
@@ -5,15 +5,23 @@
 # author：Wowooco
 # site：www.wushutong.cn
 # version：v1.0.0
-# repo：
+# repo：https://github.com/Wowooco/CloudFlare-DDNS
 #--------------------------------------------
 
 # 请将这些替换为自己的信息
+shellScript_dir="${HOME}/ddns/"
 
 function getMyIPAddr(){
     echo  
     curl ip.p3terx.com
     echo
+}
+function uninstall(){
+    echo "已卸载"
+}
+function touchShellScript(){
+    touch "${shellScript_dir}ddns.sh"
+    echo -e "\033[32m 脚本文件放在：${shellScript_dir} \033[0m"
 }
 
 function StartMenu() {
@@ -29,13 +37,14 @@ function StartMenu() {
     echo -e "\033[32m 3.运行定时 \033[0m"
     echo -e "\033[32m 4.关闭定时 \033[0m"
     echo -e "\033[32m 5.查看IP \033[0m"
+    echo -e "\033[32m 6.删除脚本 \033[0m"
     echo -e "\033[32m 0.退出 \033[0m"
     echo -e "\033[33m ==================================== \033[0m"
     #菜单结束
     read -p " 请输入数字:" inputMenuNumber
     case "$inputMenuNumber" in
             1 )
-           echo 选择1
+           touchShellScript
 	        ;;
 
             2 )
@@ -51,6 +60,9 @@ function StartMenu() {
 	        ;;
             5 )
            getMyIPAddr
+            ;;
+            6 )
+           uninstall
             ;;
             0 )
            echo -e "\033[31m 脚本已退出 \033[0m"
